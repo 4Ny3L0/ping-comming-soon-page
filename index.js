def274dc30bc260fcff7b09bfb457aae63dfd23c
Angel Gutierrez <angel50710@hotmail.com>
2022-02-04 11:44:33 -0500

    Adding js validations

--- index.js
+++ index.js
@@ -4,7 +4,33 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function validateForm(e) {
+  const message = document.querySelector(".message");
+  const emailInput = document.querySelector(".email");
   e.preventDefault();
-  const emailValue = e.target.children[0].value;
-  console.log(emailValue);
+  const emailValue = emailInput.value;
+  if (
+    emailValue === "" ||
+    emailValue === undefined ||
+    !validateEmail(emailValue)
+  ) {
+    message.textContent = "Please provide a valid email address";
+    message.classList.add("error");
+    message.style.visibility = "visible";
+    setTimeout(() => {
+      message.style.visibility = "hidden";
+      message.classList.remove("error");
+    }, 3000);
+  } else {
+    message.textContent = "Ready will contact with you soon!!";
+    message.classList.add("success");
+    message.style.visibility = "visible";
+    setTimeout(() => {
+      message.style.visibility = "hidden";
+      message.classList.remove("success");
+    }, 3000);
+  }
 }
+
+const validateEmail = (email) => {
+  return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+};
